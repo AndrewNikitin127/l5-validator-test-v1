d784c556b1ded769dd308fb396e6683f3f5281d4
AndrewNikitin127 <donandrain153@gmail.com>
2023-12-25 14:19:36 +0500

    second task complete

--- src/NumberSchema.js
+++ src/NumberSchema.js
@@ -1,8 +1,19 @@
 export default class NumberSchema {
   validators = [(val)=> Number.isFinite(val)]
 
+  constructor(newValidators){
+    if(newValidators) this.validators = newValidators;
+  }
 
   isValid(val){
     return this.validators.every((validator) => validator(val));
+  }
+  even(val){
+    const evenValidator = (val) => val % 2 === 0;
+    return new NumberSchema([...this.validators, evenValidator]);
+  }
+  odd(val){
+    const evenValidator = (val) => val % 2 !== 0;
+    return new NumberSchema([...this.validators, evenValidator]);
   }
 }
